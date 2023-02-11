d1ea54e0b15729bbaf3c7a1e8001ff7b16dd9ef3
hamraskannur <kkhamras741@gmail.com>
2023-02-11 12:14:20 +0530

    learn heapsort

--- quicknheaosrt.js
+++ quicknheaosrt.js
@@ -1,107 +1,58 @@
-const swap = (arr, left, right) =>  {
-    const temp = arr[left]
-    arr[left] = arr[right]
-    arr[right] = temp;
-  }
-  const partitionLow = (arr, low, high) => {
-    //Pick the first element as pivot
-    let pivot = arr[low];
-    let i = low;
-    
-    //Partition the array into two parts using the pivot
-    for(let j = low; j <= high; j++){
-      if(arr[j] <= pivot){ 
-        swap(arr, i, j);
-        i++;
+{
+  console.log('bubble sort');
+  function bubbleSort(arr) {
+      for (i = 0; i < arr.length; i++) {
+          for (j = 0; j < arr.length - 1 - i; j++) {
+              if (arr[j] > arr[j + 1]) {
+                  let temp = arr[j]
+                  arr[j] = arr[j + 1]
+                  arr[j + 1] = temp
+              }
+          }
       }
-    }
-    
-    swap(arr, i - 1, low);
-    
-    //Return the pivot index
-    return i - 1;
   }
-  
-  const quicksort = (arr, low, high) => {
-      // base condition
-      if (low >= high) {
-        return;
-      }
-  
-      // rearrange the elements across pivot
-      const pivot = partitionLow(arr, low, high);
-  
-      // recur on sub-array containing elements less than pivot
-      quicksort(arr, low, pivot - 1);
-  
-      // recur on sub-array containing elements more than pivot
-      quicksort(arr, pivot + 1, high);
-  }
-//   Input:
-  // let arr = [10, 7, 8, 9, 1, 5];
-  quickSort(arr, 0, arr.length - 1);
+  const arr = [8, 20, -2, 4, -6]
+  bubbleSort(arr)
   console.log(arr);
-  
-  Output:
-  [1, 5, 7, 8, 9, 10]
+}
 
+{
+  console.log('insertion sort');
 
-  const minHeapify = (arr, n, i) => {
-    let smallest = i;
-    let l = 2 * i + 1; //left child index
-    let r = 2 * i + 2; //right child index
-    
-    //If left child is smaller than root
-     if (l < n && arr[l] < arr[smallest]) {
-              smallest = l; 
-     }
-    
-     // If right child is smaller than smallest so far 
-     if (r < n && arr[r] < arr[smallest]) {
-          smallest = r; 
-     }
-    
-     // If smallest is not root 
-     if (smallest != i) { 
-          let temp = arr[i]; 
-          arr[i] = arr[smallest]; 
-          arr[smallest] = temp; 
-    
-        // Recursively heapify the affected sub-tree 
-        minHeapify(arr, n, smallest); 
-      } 
+  function insertionSort(arr) {
+      for (i = 1; i < arr.length; i++) {
+          let numbertoinsert = arr[i]
+          let j = i - 1
+          while (j >= 0 && arr[j] > numbertoinsert) {
+              arr[j + 1] = arr[j]
+              j = j - 1
+          }
+          arr[j + 1] = numbertoinsert
+      }
   }
-  
-   // main function to do heap sort 
-   const heapSort = (arr, n) => { 
-       // Build heap (rearrange array) 
-       for (let i = parseInt(n / 2 - 1); i >= 0; i--) {
-           minHeapify(arr, n, i); 
-       }
-    
-       // One by one extract an element from heap 
-       for (let i = n - 1; i >= 0; i--) { 
-          // Move current root to end 
-          let temp = arr[0]; 
-          arr[0] = arr[i]; 
-          arr[i] = temp; 
-    
-          // call max heapify on the reduced heap 
-          minHeapify(arr, i, 0); 
-       } 
-   }
+  const arr = [8, 20, -2, 4, -6]
+  insertionSort(arr)
 
-//    Input:
-const arr = [4, 6, 3, 2, 9];
-heapSort(arr, arr.length);
-console.log(arr);
+  console.log(arr);
+}
 
-Output:
-[9, 6, 4, 3, 2]
+{
+  console.log('selection sort');
 
+  function selectionSort(arr) {
+      for (i = 0; i < arr.length; i++) {
+          for (j = i + 1; j < arr.length; j++) {
+              if (arr[i] > arr[j]) {
+                  let temp = arr[i]
+                  arr[i] = arr[j]
+                  arr[j] = temp
+              }
+          }
 
+      }
+  }
+  const arr = [8, 20, -2, 4, -6]
+  selectionSort(arr)
 
-
-
-
-
+  console.log(arr);
+}
